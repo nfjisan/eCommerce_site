a71582c6fa2095bd5337bdbddafe12977611fc47
nfjisan <mdzisanahmed83@gmail.com>
2022-12-01 22:21:24 +0600

    auth and middleware and admintemplete customize mastaring and logout admin completed

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+
+
+    use AuthenticatesUsers;
+
+
+    // protected $redirectTo = RouteServiceProvider::HOME;
+
+    protected function authenticated()
+    {
+        if(Auth::user()->role_as =='1'){
+            return redirect('admin/dashboard')->with('message','Welcome to Dashboard');
+        }else{
+            return redirect('/home')->with('status','Logged In Successfully');
+        }
+    }
+
+
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function(){
+
+    Route::get('dashboard',[DashboardController::class, 'index']);
+});
